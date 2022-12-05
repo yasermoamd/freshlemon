be78109509a2b3f157ae162222b72a2ab609c325
yasermoamd <50041500@belfastmet.ac.uk>
2022-12-05 10:41:55 +0000

    onboard page

---
+++ routes/index.tsx
@@ -0,0 +1,43 @@
+import { Head } from "$fresh/runtime.ts";
+import { Link } from "preact-router";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div class="bg-[#047857]">
+        <div class="lg:absolute top-60 mx-14 py-4">
+          <div class="grid content-start md:content-around">
+            <div class="flex flex-col">
+              <span class="text-[40px]">
+                Join over 50 millions people
+                <br />
+                sharing recipes everyday
+              </span>
+              <br />
+              <span class="">
+                Never run out of ideas again. Try new foods, ingredients,
+                cooking style, and more
+              </span>
+            </div>
+            <div class="flex justify-between w-[500px] my-8 ">
+              <div class="bg-white text-[#30BE76] text-[14px]  w-[180px] text-[18px] text-center rounded-xl shadow-md">
+                <Link href="/signup" class="pr-[17px] border-none">
+                  Join Scratch
+                </Link>
+              </div>
+              <div class="">
+              </div>
+              <div class="bg-[#30BE76] p-2 text-[#ffff] w-[180px] text-[18px] text-center rounded-xl shadow-md">
+                <Link href="/about" class="pl-[17px] border-none">
+                  Learn More
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
