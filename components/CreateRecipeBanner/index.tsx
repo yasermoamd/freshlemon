75ceff7f54baeb8aca43f18815a449829357be29
yasermoamd <50041500@belfastmet.ac.uk>
2022-12-07 08:20:12 +0000

    updated with small screen

--- components/CreateRecipeBanner/index.tsx
+++ components/CreateRecipeBanner/index.tsx
@@ -6,15 +6,16 @@ type CreateRecipeProps = {
 
 const CreateRecipeBanner = (props: CreateRecipeProps) => {
 	return (
-		<div class="bg-white w-[731px] p-4 max-h-[80px] rounded-2xl">
-			<div class="flex justify-between items-center px-4">
+		<div class="bg-white w-[731px] sm:w-[481px] p-4 sm:p-4 h-[80px] rounded-2xl">
+			<div class="flex justify-between items-center ">
 				<div class="">
 					<p class="text-[#030F09] text-[18px] leading-[20px] font-serif">
 						{props.followers} of your followers are online
 					</p>
 				</div>
-				<button class="bg-[#30BE76] text-white w-[179px] h-[50px] font-serif font-bold text-[16px] text-center rounded-xl shadow-lg">
-					<Link href="/signup" class="pl-[17px]">
+				<button class="bg-[#30BE76] text-white w-[179px] sm:w-[149px] h-[50px] 
+				font-serif font-bold text-[14px] text-center rounded-xl shadow-lg">
+					<Link href="/signup">
 						Create Recipe
 					</Link>
 				</button>
